4d247482b79f02dfd641e5c42b5658b6a012182a
Fernando554 <fernando_1320114093@uptecamac.edu.mx>
2023-09-21 14:32:39 -0600

    insert components v0.1

---
+++ resources/views/livewire/gift-table-component.blade.php
@@ -0,0 +1,24 @@
+<div>
+    <section class="py-5 bg-secondary-subtle">
+        <div class="container py-5">
+            <div class="row">
+                <div class="col-12 col-lg-6 d-flex flex-column justify-content-center align-items-center text-center">
+                    <h2>
+                        Mesa de Regalos
+                    </h2>
+                    <div class="mt-4">
+                        Tenemos disponible una mesa de regalos para que puedas apoyarnos con lo que gustes.
+                    </div>
+                    <div class="mt-4">
+                        <a href="" class="btn btn-primary">
+                            Ver Mesa de Regalos
+                        </a>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6 d-flex justify-content-center align-items-center mt-4 mt-lg-0">
+                    <img src="https://1.bp.blogspot.com/-QLOY5pdwqX4/WetwCmQYNFI/AAAAAAAAYv0/idyW5Yfo8ZAe4qpOnjcz0Lsr9t3lToekACLcBGAs/s1600/liverpool.jpg" alt="" class="img-fluid object-fit-contain w-75">
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
