5a7d6e948fd101435d4caf3f59e1798a9d99cec8
Emmanuel Santoyo <90350942+EMMANUELSCH19@users.noreply.github.com>
2023-10-04 10:43:44 -0600

    Menu options to provider & client

---
+++ resources/views/layouts/users/sidebar-menu/provider.blade.php
@@ -0,0 +1,9 @@
+<li class="nav-label mg-b-15">Menú [Proveedor]</li>
+<li class="nav-item"><a href="index-2.html" class="nav-link"><i data-feather="layout"></i> Dashboard</a></li>
+<li class="nav-item">
+	<a href="#" class="nav-link with-sub"><i data-feather="layers"></i> Menú desplegable</a>
+	<nav class="nav">
+		<a href="el-accordion.html">Opción 1</a>
+		<a href="el-alerts.html">Opción 2</a>
+	</nav>
+</li>
