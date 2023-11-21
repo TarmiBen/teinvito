946208177402da7f3cc1047d5c3069fcddb9a54e
Emmanuel Santoyo <90350942+EMMANUELSCH19@users.noreply.github.com>
2023-11-21 15:02:16 -0600

    update views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,43 +1,135 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel - Livewire</title>
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        @yield('title') | Te Invito
+    </title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <link rel="stylesheet" type="text/css" href="/assets/css/main.css">
+    <link rel="stylesheet" href="/assets/css/header/main.css" />
 
-    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 
-    {{-- <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" /> --}}
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-
-    @livewireStyles
+    @yield('styles')
 </head>
 
-<body class="antialiased md:bg-gray-100 bg-purple-700">
-    <div class="text-center mt-3">
-        <div class="inline-flex" id="main_menu">
-            <ul class="flex flex-wrap p-1 md:p-2 sm:bg-gray-300 sm:rounded-full text-sm md:text-base" id="menu_nav">
-                
-                <li class="relative mx-1 px-4 py-2 group bg-gray-300 rounded-full mb-1 md:mb-0" id="button_home">
-                    <a class="font-semibold whitespace-no-wrap text-gray-600 hover:text-blue-800" href="/select2">
-                        <span class="last firstlevel">Select2 con livewire</span>
-                    </a>
-                </li>               
-
-            </ul>
+<body>
+    {{-- Header section --}}
+    <nav class="navbar navbar-expand-lg fixed-top bg-deg-quaternary-3 m-0 rounded-bottom shadow-lg">
+        <div class="container">
+            <a class="navbar-brand text-white me-auto" href="#">Te Invito</a>
+            <div class="offcanvas offcanvas-end bg-dark" tabindex="-1" id="offcanvasNavbar"
+                aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title text-white" id="offcanvasNavbarLabel">Te Invito</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
+                        <li class="nav-item">
+                            <a class="nav-link text-white mx-lg-2 active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white mx-lg-2" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white mx-lg-2" href="#">Services</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white mx-lg-2" href="#">Portfolio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white mx-lg-2" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <a href="#" class="login-button me-4 text-white">Login</a>
+            <button class="navbar-toggler bg-secondary-ti" type="button" data-bs-toggle="offcanvas"
+                data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
         </div>
-    </div>
-    <main class="container mx-auto">
-        @yield('content')
-    </main>
+    </nav>
 
-    @livewireScripts
+
+    <div class="mt-1 pt-5">
+        @yield('content')
+    </div>
+
+
+    {{-- footer section --}}
+    <footer class="py-5 bg-deg-tertiary-3 text-white">
+        <div class="">
+            <div class="container">
+                <div class="">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-6 col-12">
+                            <div class="">
+                                <div class="">
+                                    <h1>
+                                        Te Invito
+                                    </h1>
+                                </div>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum
+                                    voluptates. Quisquam, voluptatum voluptates.
+                                </p>
+                                <p class="copyright-text">
+                                    Diseñado y desarrollado por
+                                    <a href="" class="link-light">
+                                        Dratenz SS
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-lg-2 col-md-6 col-12">
+                            <div class="">
+                                <h3>Solutions</h3>
+                                <div class="d-flex flex-column">
+                                    <a href="" class="link-light text-decoration-none ">Marketing</a>
+                                    <a href="" class="link-light text-decoration-none ">Analytics</a>
+                                    <a href="" class="link-light text-decoration-none ">Commerce</a>
+                                    <a href="" class="link-light text-decoration-none ">Insights</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-2 col-md-6 col-12">
+                            <div class="">
+                                <h3>Support</h3>
+                                <div class="d-flex flex-column">
+                                    <a href="" class="link-light text-decoration-none ">Pricing</a>
+                                    <a href="" class="link-light text-decoration-none ">Documentation</a>
+                                    <a href="" class="link-light text-decoration-none ">Guides</a>
+                                    <a href="" class="link-light text-decoration-none ">API Status</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6 col-12">
+                            <div class="">
+                                <h3>Subscribe</h3>
+                                <p>Subscribe to our newsletter for the latest updates</p>
+                                <form action="#" method="post" target="_blank" class="">
+                                    <input name="email" placeholder="Email address" required="required" type="email"
+                                        class="form-control text-center">
+                                    <div class="mt-2">
+                                        <button class="btn btn-secondary-ti w-100">
+                                            <i class="fa fa-paper-plane" aria-hidden="true"></i>
+                                            Subscribirme
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
 
     @yield('scripts')
 </body>
--- resources/views/users/subscription/index.blade.php
+++ resources/views/users/subscription/index.blade.php
@@ -1,69 +1,55 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Paquetes</title>
-    
-    <!-- Icons-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    
-    <!-- Styles CSS -->
-    @vite(['resources/sass/app.scss'])
-	<link rel="stylesheet" type="text/css" href="/assets/css/style.css">
-	<link rel="stylesheet" type="text/css" href="/assets/css/main.css">
+@extends('layouts.app')
 
-</head>
-<body>
+@section('title', 'Paquetes')
+
+@section('content')
+
     <div class="container py-5">
         <h2>
-            <i class="fa fa-list" aria-hidden="true"></i> 
+            <i class="fa fa-list" aria-hidden="true"></i>
             Paquetes
         </h2>
-    
-        <div class="row row-cols-4 g-3 justify-content-center mt-3">
-            @for($i = 0; $i < 5; $i++)
-            <div class="col">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="card-title text-center py-2">
-                            <h3>
-                                Paquete X
-                            </h3>
-                        </div>
-    
-                        <div class="text-center fs-3 fw-bold">
-                            $300/mes
-                        </div>
-                        
-                        <div class="mt-4">
-                            <div class="d-flex">
-                                <span>
-                                    <i class="fas fa-check-circle text-success"></i>
-                                </span>
-                                <span class="ms-2">Plantilla con maximo 8 componentes</span>
+
+        <div class="row row-cols-2 row-cols-md-3 row-cols-lg-4 g-3 justify-content-center mt-3">
+            @for ($i = 0; $i < 5; $i++)
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="card-title text-center py-2">
+                                <h3>
+                                    Paquete X
+                                </h3>
                             </div>
-                            <div class="d-flex">
-                                <span>
-                                    <i class="fas fa-check-circle text-success"></i>
-                                </span>
-                                <span class="ms-2">10 componentes</span>
+
+                            <div class="text-center fs-3 fw-bold">
+                                $300/mes
                             </div>
-                        </div>
-    
-                        <div class="mt-4">
-                            <a href="{{ route('subscription.show', 1) }}" class="btn btn-quaternary-ti w-100">
-                                <i class="fa fa-shopping-cart" aria-hidden="true"></i> 
-                                Comprar
-                            </a>
+
+                            <div class="mt-4">
+                                <div class="d-flex">
+                                    <span>
+                                        <i class="fas fa-check-circle text-success"></i>
+                                    </span>
+                                    <span class="ms-2">Plantilla con maximo 8 componentes</span>
+                                </div>
+                                <div class="d-flex">
+                                    <span>
+                                        <i class="fas fa-check-circle text-success"></i>
+                                    </span>
+                                    <span class="ms-2">10 componentes</span>
+                                </div>
+                            </div>
+
+                            <div class="mt-4">
+                                <a href="{{ route('subscription.show', 1) }}" class="btn btn-quaternary-ti w-100">
+                                    <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                                    Comprar
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
             @endfor
         </div>
     </div>
-</body>
-</html>
-
-
+@endsection
